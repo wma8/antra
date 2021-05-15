335464b0d49f0db83327e81af26a31e0cdd7d1e3
MyMac <mymac@MacBook-Pro-2.local>
2021-05-15 14:28:04 -0400

    first commit

---
+++ ClientService/src/main/java/com/antra/report/client/configuration/AmazonConfiguration.java
@@ -0,0 +1,9 @@
+package com.antra.report.client.configuration;
+
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfiguration {
+
+}
